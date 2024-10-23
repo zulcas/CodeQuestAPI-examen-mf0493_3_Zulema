521f0804389571f3baaef59f63881c0421ea34c6
SharmarAndrey <sharnarandrey@gmail.com>
2024-10-23 19:53:13 +0200

    dependensis add

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 
 app.get('/api/v1/question/random', (req, res) => {
-    res.json({
-        "question": "What is the result of `2 + 2` in JavaScript?",
-        "options": [
-            { "text": "3", "correct": false },
-            { "text": "4", "correct": true },
-            { "text": "22", "correct": false },
-            { "text": "NaN", "correct": false }
-        ]
-    })
+	res.json({
+		"question": "What is the result of `2 + 2` in JavaScript?",
+		"options": [
+			{ "text": "3", "correct": false },
+			{ "text": "4", "correct": true },
+			{ "text": "22", "correct": false },
+			{ "text": "NaN", "correct": false }
+		]
+	})
 })
 
 app.listen(3000);
