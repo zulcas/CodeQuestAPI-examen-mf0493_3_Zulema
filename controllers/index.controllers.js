e172490a1a39af78fc5d65df8ceb2c7ba5505b36
SharmarAndrey <sharnarandrey@gmail.com>
2024-10-30 21:34:12 +0100

    random questions with query string (not finished)

--- controllers/index.controllers.js
+++ controllers/index.controllers.js
@@ -2,8 +2,17 @@
 const questionService = require('../services/question.services')
 const getRandomQuestions = async (req, res) => {
 	try {
+		let { amount } = req.query;
+		amount = parseInt(amount, 10);
 
-		const randomQuestion = await questionService.getRandomQuestion();
+		//validation of amount
+		if (isNaN(amount) || amount < 1) {
+			amount = 10;
+		} else if (amount > 30) {
+			amount = 30;
+		}
+
+		const randomQuestion = await questionService.getRandomQuestion(amount);
 
 
 		/* 	const totalQuestions = await Questions.countDocuments();
@@ -16,13 +25,13 @@ const getRandomQuestions = async (req, res) => {
 
 
 		res.status(200).json({
-			message: "Random question delivered successfully",
+			message: "Random questions delivered successfully",
 			results: randomQuestion
 		});
 
 	} catch (error) {
 		res.status(500).json({
-			message: "Error fetching random question",
+			message: "Error fetching random questions",
 
 
 		})
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,8 @@
 const express = require('express');
-
 const router = express.Router();
 const indexControllers = require('../controllers/index.controllers');
 
 router.get('/api/v1/question/random', indexControllers.getRandomQuestions);
-
-
-
 
 module.exports = router;
 
