a01aa836e9187b5584f441c76f0da2e061962e00
SharmarAndrey <sharnarandrey@gmail.com>
2024-10-30 21:57:22 +0100

    random questions  amount finished

--- services/question.services.js
+++ services/question.services.js
@@ -3,12 +3,6 @@ const Questions = require('../models/question.model');
 const getRandomQuestion = async (amount) => {
 
 
-	/* const questions = await Questions.find();
-		const randomIndex = Math.floor(Math.random() * questions.length);
-		const randomQuestion = questions[randomIndex];
-		return randomQuestion; */
-
-
 	const questions = await Questions.aggregate([
 		{ $sample: { size: amount } }
 	]);
