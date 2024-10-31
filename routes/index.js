f1d2c673888132a23ac8324c6c0e57c7800844d8
Lorena Criado <criado.manzaneque@gmail.com>
2024-10-31 21:10:06 +0100

    Testing the endpoint

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const indexControllers = require('../controllers/index.controllers');
 
-router.get('/api/v1/question/random', indexControllers.getRandomQuestions);
+router.get('/api/v1/questions/random', indexControllers.getRandomQuestions);
 
 module.exports = router;
 
