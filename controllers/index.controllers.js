f4b7d330e1a8a825bdbbff9ec8367d6a9677b1a7
SharmarAndrey <sharnarandrey@gmail.com>
2024-11-04 20:50:50 +0100

    validation for GetRandomQuestion

--- controllers/index.controllers.js
+++ controllers/index.controllers.js
@@ -1,32 +1,27 @@
-
-const questionService = require('../services/question.services')
+const questionService = require("../services/question.services");
 const getRandomQuestions = async (req, res) => {
-	try {
-		let { amount } = req.query;
-		amount = parseInt(amount, 10);
+  try {
+    let { amount } = req.query;
+    amount = parseInt(amount, 10);
 
-		//validation of amount
-		if (isNaN(amount) || amount < 1) {
+    //validation of amount
+    /* if (isNaN(amount) || amount < 1) {
 			amount = 10;
 		} else if (amount > 30) {
 			amount = 30;
-		}
+		} */
 
-		const randomQuestion = await questionService.getRandomQuestion(amount);
+    const randomQuestion = await questionService.getRandomQuestion(amount);
 
-		res.status(200).json({
-			message: "Random questions delivered successfully",
-			results: randomQuestion
-		});
-
-	} catch (error) {
-		res.status(500).json({
-			message: "Error fetching random questions",
-
-
-		})
-	}
-
-}
+    res.status(200).json({
+      message: "Random questions delivered successfully",
+      results: randomQuestion,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error fetching random questions",
+    });
+  }
+};
 
 module.exports = { getRandomQuestions };
--- services/question.services.js
+++ services/question.services.js
@@ -1,16 +1,14 @@
-const Questions = require('../models/question.model');
+const Questions = require("../models/question.model");
 
 const getRandomQuestion = async (amount) => {
+  if (typeof amount != number || amount <= 0) {
+    throw new Error("Amount must be a positive number.");
+  }
 
-
-	const questions = await Questions.aggregate([
-		{ $sample: { size: amount } }
-	]);
-	return questions;
+  const questions = await Questions.aggregate([{ $sample: { size: amount } }]);
+  return questions;
 };
 
-
-
 module.exports = {
-	getRandomQuestion
-}
+  getRandomQuestion,
+};
