90a648600f35c24a655b837698c78ffd8494765a
kevmhughes <kevhughes24@hotmail.com>
2024-11-04 13:54:21 +0100

    Import getQuestionsFromAI from services and the Questions model.

--- controllers/index.controllers.js
+++ controllers/index.controllers.js
@@ -1,5 +1,8 @@
-
 const questionService = require('../services/question.services')
+const { getQuestionsFromAI } = require("../services/question.services")
+const Questions = require("../models/question.model");
+
+
 const getRandomQuestions = async (req, res) => {
 	try {
 		let { amount } = req.query;
@@ -29,4 +32,48 @@ const getRandomQuestions = async (req, res) => {
 
 }
 
-module.exports = { getRandomQuestions };
+// Logic to get the generated question from the AI
+const getAiQuestions = async (req, res) => {
+	const topic = req.query.topic || "Frontend and Backend programming";
+	if (topic.length < 2 || topic.length > 140) {
+	  return res
+		.status(400)
+		.json({
+		  error:
+			"Topic must be at least 2 characters and not exceed 140 characters.",
+		});
+	}
+  
+	const amount = Math.min(Math.max(parseInt(req.query.amount) || 1, 1), 10);
+	const questions = [];
+  
+	try {
+	  for (let i = 0; i < amount; i++) {
+		const quizData = await getQuestionsFromAI(topic);
+		questions.push({
+		  ...quizData,
+		  status: "pending",
+		});
+	  }
+  
+	  await Questions.insertMany(questions);
+  
+	  return res.status(200).json({
+		message: "Random question delivered successfully",
+		results: questions,
+	  });
+	} catch (error) {
+	  console.error(
+		"Error generating the question:",
+		error.response && error.response.data
+		  ? error.response.data
+		  : error.message
+	  );
+	}
+  };
+
+  
+module.exports = { 
+	getRandomQuestions,
+	getAiQuestions
+};
