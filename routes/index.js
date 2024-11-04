e7dad0bd330edfefe423b330606aa4713ddf1d7b
kevmhughes <kevhughes24@hotmail.com>
2024-11-04 13:54:51 +0100

    Add '/api/v1/question/ai' route.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+
 const router = express.Router();
 const indexControllers = require('../controllers/index.controllers');
 
-router.get('/api/v1/questions/random', indexControllers.getRandomQuestions);
+router.get('/api/v1/question/random', indexControllers.getRandomQuestions);
+
+router.get('/api/v1/question/ai', indexControllers.getAiQuestions) 
 
 module.exports = router;
 
