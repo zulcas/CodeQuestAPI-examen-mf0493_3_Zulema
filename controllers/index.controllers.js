bf4645bfdb40243c38a5a78fdf27b09d79dc0ef1
SharmarAndrey <sharnarandrey@gmail.com>
2024-11-05 19:29:36 +0100

    JSDOC for sercices and controllers

--- controllers/index.controllers.js
+++ controllers/index.controllers.js
@@ -1,4 +1,23 @@
 const questionService = require("../services/question.services");
+
+/**
+ * Get random questions from the service.
+ *
+ * This function retrieves a specified number of random questions from the question service.
+ * It validates the 'amount' query parameter, ensuring it is a valid number between 1 and 30.
+ * If 'amount' is not provided or is invalid, a default value of 10 is used. The maximum number
+ * of questions that can be requested is capped at 30 to prevent overloading the service.
+ *
+ * @async
+ * @function getRandomQuestions
+ * @param {Object} req - Express request object.
+ * @param {Object} req.query - Query parameters from the request.
+ * @param {string} req.query.amount - The number of questions requested. This parameter is optional.
+ * @param {Object} res - Express response object used to send back the response.
+ * @returns {Promise<void>} Sends a JSON response with the requested random questions or an error message.
+ *
+ * @throws {Error} If an unexpected error occurs, a 500 status code is returned with the error message.
+ */
 const getRandomQuestions = async (req, res) => {
   try {
     let { amount } = req.query;
@@ -19,7 +38,7 @@ const getRandomQuestions = async (req, res) => {
     });
   } catch (error) {
     res.status(500).json({
-      message: "Error fetching random questions",
+      message: error.message,
     });
   }
 };
